c9a12206e6dbecad52e3fb8974df3c19b26d6ad3
AtharvaJ1010 <146018737+AtharvaJ1010@users.noreply.github.com>
2025-04-17 13:47:23 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,12 @@
-// Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCjpdU9n5tD7t8GSDRtY2hfBgqBQN0n9ZI",
-    authDomain: "labassignment-7da64.firebaseapp.com",
-    projectId: "labassignment-7da64",
-    storageBucket: "labassignment-7da64.appspot.com",
-    messagingSenderId: "796889797175",
-    appId: "1:796889797175:web:7e506386f815ef9f08e5b2",
-    measurementId: "G-MKHMJX56D1"
+  apiKey: "AIzaSyCjpdU9n5tD7t8GSDRtY2hfBgqBQN0n9ZI",
+  authDomain: "labassignment-7da64.firebaseapp.com",
+  projectId: "labassignment-7da64",
+  storageBucket: "labassignment-7da64.firebasestorage.app",
+  messagingSenderId: "796889797175",
+  appId: "1:796889797175:web:d951fbb3a2261b0e08e5b2",
+  measurementId: "G-NJFGB24644"
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
